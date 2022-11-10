43fda3fb72acaf9ad18222a32a424dd1cef02979
JonatasSilva01 <jhownsantossilva@gmail.com>
2022-11-10 13:40:28 -0300

    atualizando a branch Object-types

--- src/index.js
+++ src/index.js
@@ -3,22 +3,48 @@ import { ApolloServer, gql } from 'apollo-server';
 const server = new ApolloServer({
   typeDefs: gql`
     type Query {
+      user: User!
+      users: [Users!]!
+    }
+    type User {
       id: ID
       name: String
       age: Int
-      average: Float
-      married: Boolean
-      car: [String!]!
+    }
+    type Users {
+      id: ID
+      name: String
+      age: Int
     }
   `,
   resolvers: {
     Query: {
-      id: () => 'asdvf-1',
-      name: () => 'Jonatas Silva',
-      age: () => 21,
-      average: () => 50.55,
-      married: () => false,
-      car: () => ['gol-bolinha', 'gol-quadrado', 'mustang-GC500'],
+      user: () => {
+        return {
+          id: 'abcd-123',
+          name: 'Jonatas',
+          age: 21,
+        };
+      },
+      users: () => {
+        return [
+          {
+            id: 'abcd-123',
+            name: 'Jonatas',
+            age: 21,
+          },
+          {
+            id: 'abcd-121',
+            name: 'Henrique',
+            age: 21,
+          },
+          {
+            id: 'abcd-125',
+            name: 'Nicolas',
+            age: 21,
+          },
+        ];
+      },
     },
   },
 });
